ae0a18eaac430f5607c3ddbbc6b1f06a09928292
Bernardo Trindade <btrindade@eeshoo.com>
2013-07-03 17:22:27 -0600

    edited user update function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
