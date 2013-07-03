b322090f9b7c1912c8a0e684069e43743ee911d6
Bernardo Trindade <btrindade@eeshoo.com>
2013-07-03 17:33:25 -0600

    edited user controller to store desired uri for friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,7 @@ class UsersController < ApplicationController
   private
     def signed_in_user
       unless signed_in?
+        store_location
         flash[:notice] = "Please sign in."
         redirect_to signin_url
       end
